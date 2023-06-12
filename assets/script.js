d7a94c855230f7262f9d1ed31aae7e70c35f5e2c
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-06-12 15:10:52 -0500

    failed attempt at fetch

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,6 @@
 console.log('hello');
 
 
-var requestUrl = ('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}')
 
-function getApi(requestUrl) {
-    fetch(requestUrl)
-      .then(function (response) {
-        console.log(response); }
-      
-      )}
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={8786096495ea01934e61f3b183f55c15}')
+  .then(res => console.log(res))
